9e4dc88774715b5370f01cd1fb8a72a5f1ddb2ec
Tyler <68185249+tywil04@users.noreply.github.com>
2022-07-11 14:09:02 +0100

    remove devise

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,19 +1,13 @@
 class TodosController < ApplicationController
   before_action :set_todo, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!
 
   # GET /todos or /todos.json
   def index
-    @todos = Todo.where(user: current_user)
+    @todos = Todo.all
   end
 
   # GET /todos/1/edit
   def edit
-    if @todo.user != current_user
-      respond_to do |format|
-        format.html { redirect_to todos_path }
-      end
-    end
   end
 
   # POST /todos or /todos.json
@@ -29,23 +23,19 @@ class TodosController < ApplicationController
 
   # PATCH/PUT /todos/1 or /todos/1.json
   def update
-    if @todo.user == current_user
-      respond_to do |format|
-        if @todo.update(todo_params)
-          format.turbo_stream
-        end
+    respond_to do |format|
+      if @todo.update(todo_params)
+        format.turbo_stream
       end
     end
   end
 
   # DELETE /todos/1 or /todos/1.json
   def destroy
-    if @todo.user == current_user
-      @todo.destroy
+    @todo.destroy
 
-      respond_to do |format|
-        format.turbo_stream
-      end
+    respond_to do |format|
+      format.turbo_stream
     end
   end
 
@@ -57,6 +47,6 @@ class TodosController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def todo_params
-      params.fetch(:todo, {}).permit(:title, :completed).merge("user" => current_user)
+      params.fetch(:todo, {}).permit(:title, :completed)
     end
 end
